6e4150585294bf1402ba8df5088c60cb28a8317f
Ajay Shenoy P <154616179+AS-ciii@users.noreply.github.com>
2024-05-23 20:22:26 +0530

    assets

---
+++ src/assets/assets.js
@@ -0,0 +1,21 @@
+import history_icon from './history_icon.png'
+import menu_icon from './menu_icon.png'
+import plus_icon from './plus_icon.png'
+import bulb_icon from './bulb_icon.png'
+import user_icon from './user_icon.png'
+import message_icon from './message_icon.png'
+import code_icon from './code_icon.png'
+import send_icon from './send_icon.png'
+import orate_logo from './orate_logo.png'
+
+export const assets = {
+    history_icon,
+    menu_icon,
+    plus_icon,
+    bulb_icon,
+    user_icon,
+    message_icon,
+    code_icon,
+    send_icon,
+    orate_logo
+}
